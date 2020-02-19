33fb396c28c0535b6b8812c9a027568a689dbdb3
Kragrathea <maildropdrop@gmail.com>
2020-02-19 00:10:27 -0800

    Add capture support

--- PinFEApp/routes/index.js
+++ PinFEApp/routes/index.js
@@ -89,10 +89,44 @@ router.post('/play', function (req, res) {
 
     console.log("POST play table:" + req.body.table);
     //var cmd = '"f:/Games/Visual Pinball/VPinballX_cab.exe" /play ' + '"c:\\' + tableDir + req.body.table + '"';
+
     runPlayer('../../VisualPinball/VisualPinballCab.exe', ["/play", tableDir + req.body.table, arg,"-minimized"]);
+
+    //captureVideo('../../VisualPinball/VisualPinballCab.exe', ["/play", tableDir + req.body.table, arg, "-minimized"], tableDir + req.body.table+".bg.mp4");
 
     res.end();
 });
+router.post('/capture', function (req, res) {
+    var query = url.parse(req.url, true).query;
+    var view = query.view;
+
+    console.log("POST capture table:"+view +" " + req.body.table);
+
+    if (view) {
+        var arg = "";
+        var outName;
+        if (view && view.toLowerCase() === "dt") {
+            arg = "-ForceDT";
+            outName = tableDir + req.body.table + ".dt.mp4"
+        } else if (view && view.toLowerCase() === "fss") {
+            arg = "-ForceFSS";
+            outName = tableDir + req.body.table + ".fss.mp4"
+        } else if (view && view.toLowerCase() === "bg") {
+            arg = "-ForceFS";
+            outName = tableDir + req.body.table + ".bg.mp4"
+        } else if (view && view.toLowerCase() === "fs") {
+            arg = "-ForceFS";
+            outName = tableDir + req.body.table + ".fs.mp4"
+        }
+        if(arg!=="")
+            captureVideo('../../VisualPinball/VisualPinballCab.exe', ["/play", tableDir + req.body.table, arg, "-minimized"], outName);
+    }
+
+    res.end();
+});
+
+
+
 var curSelectedTable = "";
 router.post('/select', function (req, res) {
     console.log(req.body);
@@ -131,6 +165,125 @@ router.get('/status', function (req, res) {
     sendStatus(res);
 });
 
+var captureActive = false;
+function captureVideo(command, args, outName) {
+    console.log("Starting capture:"+args);
+    if (playerActive)
+        return;
+
+    console.log("Capture outName:" + outName);
+    var child = child_process.spawn(command, args);
+    playerActive = true;
+    child.on('error', function (err) {
+        console.log('Error running player: ' + err);
+        playerActive = false;
+    });
+
+    var scriptOutput = "";
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+
+        if (data.toString().indexOf("VPTableStart")>-1) {
+
+            var ffargs = ["-loglevel", "error", "-stats", "-f", "gdigrab", "-framerate", "30", "-offset_x", "-1920", "-offset_y", "0",
+                "-video_size", "1920x1080", "-ss", "6", "-t", "00:00:2", "-i", "desktop", "-c:v", "h264_nvenc", "-an", "-preset", "lossless", "-y", "-f", "mp4",
+                outName+"_"];
+
+            var ffmpeg = child_process.spawn("../../Ffmpeg/ffmpeg.exe", ffargs);
+            captureActive = true;
+            ffmpeg.on('error', function (err) {
+                console.log('Error running player: ' + err);
+                captureActive = false;
+            });
+
+            ffmpeg.stdout.setEncoding('utf8');
+            ffmpeg.stdout.on('data', function (data) {
+                console.log('ffmpeg stdout: ' + data);
+            });
+
+            ffmpeg.stderr.setEncoding('utf8');
+            ffmpeg.stderr.on('data', function (data) {
+                console.log('ffmpeg stderr: ' + data);
+
+                //data = data.toString();
+                //scriptOutput += data;
+            });
+
+            ffmpeg.on('close', function (code) {
+                //kill vp
+                child.kill();
+
+
+                let ffREargs = ["-i", outName + "_", "-loglevel", "error", "-stats", "-c:v", "libx264", "-an", "-crf", "20", "-preset", "slower", "-pix_fmt", "yuv420p", "-y", "-f", "mp4", outName];
+                let ffREmpeg = child_process.spawn("../../Ffmpeg/ffmpeg.exe", ffREargs);
+
+                ffREmpeg.stdout.setEncoding('utf8');
+                ffREmpeg.stdout.on('data', function (data) {
+                    console.log('ffmpeg stdout: ' + data);
+                });
+                ffREmpeg.on('error', function (err) {
+                    console.log('Error running ffmpeg: ' + err);
+                });
+
+
+                ffREmpeg.on('close', function (code) {
+                    console.log('RE Encode finished: ' + data);
+                    fs.unlinkSync(outName + "_");
+
+                    let picName = outName.replace(".mp4", ".jpg");
+                    let ffPicargs = ["-ss", "1", "-i", outName, "-vframes", "1", "-q:v", "2", "-loglevel", "error", "-y", "-stats", picName];
+                    let ffPicmpeg = child_process.spawn("../../Ffmpeg/ffmpeg.exe", ffPicargs);
+
+                    ffPicmpeg.stdout.on('data', function (data) {
+                        console.log('ffmpeg stdout: ' + data);
+                    });
+                    ffPicmpeg.on('error', function (err) {
+                        console.log('Error running ffmpeg: ' + err);
+                    });
+                    ffPicmpeg.on('close', function (code) {
+                        console.log('FINISHED PIC: ');
+
+                        //ffPicargs = ["-i", picName, "-vf", "scale=480:-1", picName.replace(".jpg", "-small.jpg")];;
+                        //ffPicmpeg = child_process.spawn("../../Ffmpeg/ffmpeg.exe", ffPicargs);
+                    });
+
+
+
+                });
+
+                //and any backglasses running. 
+                //var bgkiller = child_process.spawn("taskkill", ["/IM", "B2SBackglassServerEXE.exe", "/F"]);
+
+                //bgkiller.stderr.setEncoding('utf8');
+                //bgkiller.stderr.on('data', function (data) {
+                //    console.log('ffmpeg stderr: ' + data);
+                //});
+
+
+                captureActive = false;
+            });
+
+
+
+
+        }
+
+    });
+
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+
+        data = data.toString();
+        scriptOutput += data;
+    });
+
+    child.on('close', function (code) {
+        playerActive = false;
+        //callback(scriptOutput, code);
+    });
+}
 
 
 function runPlayer(command, args, callback) {
