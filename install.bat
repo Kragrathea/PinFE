d42634652125ee7db5a7f7876362de51d9ff8b25
unknown <kragrathea>
2020-09-29 14:38:42 -0700

    Clean up packages

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 Echo Install Node Packages
 pushd .
-npm install
+CMD /C npm install
 pause
 cd PinFEApp
-npm install
+CMD /C npm install
 pause
 popd
 
@@ -17,8 +17,8 @@ Echo DMD Setup
 FlexDMDUI.exe
 pause
 cd ..\BackglassServer
+Echo Register Backglass. NOTE:Might have to be done as Administrator.
 B2SBackglassServerRegisterApp.exe
 popd
 Echo All Done!
 pause
-
