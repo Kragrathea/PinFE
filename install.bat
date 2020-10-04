66a370d037c89bc4dfaa281420772ef6b18252a8
unknown <kragrathea>
2020-10-04 14:50:41 -0700

    Add vp test to install

--- install.bat
+++ install.bat
@@ -1,7 +1,5 @@
 Echo Install Node Packages
 pushd .
-REM CMD /C npm install
-REM pause
 cd PinFEApp
 CMD /C npm install
 pause
@@ -9,11 +7,13 @@ popd
 
 Echo VPinMame Setup
 Echo Make sure Roms folder is set correctly
+Echo Set default Rom options
 pushd .
 cd Apps\VPinMAME
 setup.exe
 pause
-Echo DMD Setup.Ignore the first red X. Register everything. 
+Echo DMD Setup. Ignore the first red X. Register everything. 
+Echo Possibly setup DMD positions. 
 FlexDMDUI.exe
 pause
 cd ..\BackglassServer
@@ -21,5 +21,10 @@ B2SBackglassServerRegisterApp.exe
 start .
 Echo Register Backglass by running B2SBackglassServerRegisterApp.exe. NOTE:Might have to be done as Administrator.
 popd
+pause
+Echo Configure Backglass positions
+B2S_SetUp.exe
+Echo Test and configure VP. Video options, keys, etc.
+.\Apps\VisualPinball\VisualPinballCab.exe -forceFS /play ".\Tables\Test\Nudge Test and Calibration\Nudge Test and Calibration.vpx"
 Echo All Done!
 pause
