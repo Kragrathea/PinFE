50ecc9e5d614a3e161b813f43b8996373788d876
Kragrathea <maildropdrop@gmail.com>
2022-07-13 23:50:26 -0700

    Fix update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,15 @@
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Update from Git. Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo Updating from repo
 rem git stash
 rem git fetch
 rem git pull
 rem git stash pop
+
+:END
+endlocal
+
