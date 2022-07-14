4608e77e69799069e557dca11c13f4735e26b730
Kragrathea <maildropdrop@gmail.com>
2022-07-13 23:55:22 -0700

    Fix update

--- update.bat
+++ update.bat
@@ -5,10 +5,10 @@ SET /P AREYOUSURE=Update from Git. Are you sure (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 echo Updating from repo
-rem git stash
-rem git fetch
-rem git pull
-rem git stash pop
+git stash
+git fetch
+git pull
+git stash pop
 
 :END
 endlocal
